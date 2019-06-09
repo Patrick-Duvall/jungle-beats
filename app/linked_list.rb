4bffec9ab29877b6c3a971bb6c03641dda5ed1bb
Patrick Duvall <duvallp208@gmail.com>
2019-06-09 14:26:52 -0600

    Add through find with edge cases

--- app/linked_list.rb
+++ app/linked_list.rb
@@ -6,8 +6,72 @@ class LinkedList
   end
 
   def append(data)
-    node = Node.new(data)
-    @head = node
+    if @head.nil?
+      @head = Node.new(data)
+    else
+      head = @head
+      until head.last?
+        head = head.next_node
+      end
+      head.next_node = Node.new(data)
+    end
+  end
+
+  def prepend(data)
+    old_head = @head
+    @head = Node.new(data)
+    @head.next_node = old_head
+  end
+
+  def pop
+    return false if @head.nil?
+    current = @head
+    until current.last?
+      last = current
+      current = current.next_node
+    end
+    retval = current.data
+    last.next_node = nil
+    retval
+  end
+
+  def insert(data, num)
+    return if num > count || num < 0
+    return prepend(data) if num == 0
+    current = @head
+    (num - 1).times do
+      current = current.next_node
+    end
+      old_next = current.next_node
+      current.next_node = Node.new(data)
+      current.next_node.next_node = old_next
+  end
+
+  def includes?(data)
+    return false if @head.nil?
+    current = @head
+    until current.last?
+      return true if data == current.data
+      current = current.next_node
+    end
+    return true if data == current.data
+    false
+  end
+
+  def find(start,num)
+    return '' if @head.nil?
+    return 'please enter a start index and number to select' if start < 0 || num > count - start
+    string = ''
+    current = @head
+    (start ).times do
+      current = current.next_node
+    end
+    string += current.data
+    (num -1 ).times do
+      current = current.next_node
+      string += " #{current.data}"
+    end
+    string
   end
 
   def count
@@ -23,10 +87,11 @@ class LinkedList
 
   def to_string
     return '' if @head.nil?
+    arr = Array.new
     string = ''
     head = @head
     until head.last?
-      string += head.data
+      string += "#{head.data} "
       head = head.next_node
     end
     string += head.data
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -10,19 +10,93 @@ describe LinkedList do
     expect(@list.head).to be_a(Node)
     expect(@list.head.data).to eq('bop')
     expect(@list.head.next_node).to eq(nil)
+    @list.append('beep')
+    expect(@list.head.next_node.data).to eq('beep')
+    expect(@list.head.next_node.next_node).to eq(nil)
+  end
+  it "prepend" do
+    @list.append('cop')
+    @list.append('dop')
+    @list.prepend('bop')
+    expect(@list.count).to eq(3)
+    expect(@list.to_string).to eq('bop cop dop')
+    @list.prepend('aop')
+    expect(@list.to_string).to eq('aop bop cop dop')
   end
 
   it'count' do
     expect(@list.count).to eq(0)
     @list.append('pop')
     expect(@list.count).to eq(1)
+    @list.append('coop')
+    expect(@list.count).to eq(2)
   end
 
   it 'to_string' do
     @list.append('pop')
     expect(@list.to_string).to eq('pop')
+    @list.append('pip')
+    expect(@list.to_string).to eq('pop pip')
   end
 
+  it 'insert' do
+    @list.append('cop')
+    @list.append('dop')
+    @list.prepend('bop')
+    @list.prepend('aop')
+    @list.insert('bup', 2)
+    expect(@list.to_string).to eq('aop bop bup cop dop')
+    @list.insert('cup', 4)
+    expect(@list.to_string).to eq('aop bop bup cop cup dop')
+    @list.insert('woop', 6)
+    expect(@list.to_string).to eq('aop bop bup cop cup dop woop')
+    @list.insert('bup', 9)
+    expect(@list.to_string).to eq('aop bop bup cop cup dop woop')
+    @list.insert('bup', -2)
+    expect(@list.to_string).to eq('aop bop bup cop cup dop woop')
+  end
+
+  it "insert at 0th" do
+    @list.insert('cup', 0)
+    expect(@list.to_string).to eq('cup')
+    @list.append('dop')
+    @list.insert('awg', 0)
+    expect(@list.to_string).to eq('awg cup dop')
+  end
+
+  it "find" do
+      @list.append('cop')
+      @list.append('dop')
+      @list.prepend('bop')
+      @list.prepend('aop')
+      @list.append('eop') #'aop bop cop dop eop'
+      expect(@list.find(1,1)).to eq('bop')
+      expect(@list.find(2,2)).to eq('cop dop')
+      expect(@list.find(0,3)).to eq('aop bop cop')
+      expect(@list.find(0,3)).to eq('aop bop cop')
+      expect(@list.find(-1,3)).to eq('please enter a start index and number to select')
+      expect(@list.find(0,6)).to eq('please enter a start index and number to select')
+
+  end
+  it ".pop" do
+    @list.append('cop')
+    @list.append('dop')
+    @list.prepend('bop')
+    @list.prepend('aop')
+    expect(@list.pop).to eq('dop')
+    expect(@list.to_string).to eq('aop bop cop')
+    expect(@list.pop).to eq('cop')
+    expect(@list.to_string).to eq('aop bop')
+  end
+  it "includes?" do
+    @list.append('cop')
+    @list.append('dop')
+    @list.prepend('bop')
+    @list.prepend('aop')
+    expect(@list.includes?('dop')).to eq(true)
+    expect(@list.includes?('aop')).to eq(true)
+    expect(@list.includes?('fop')).to eq(false)
+  end
 
 
 end
